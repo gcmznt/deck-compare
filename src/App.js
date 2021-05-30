b8fb9d60aa7411f5d885b84120cfa0b79212397c
Giacomo Zinetti <giacomo.zinetti@giko.it>
2021-05-30 15:01:55 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,164 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { DeckCompare } from "./deck-compare";
+
+function Card({ card, quantity }) {
+  const status = (() => {
+    if (card.quantity > card.availability) return "danger";
+    if (card.decks > 1 && card.info.is_unique) return "warning";
+    return "ok";
+  })();
+
+  return quantity ? (
+    <div title={card.info.name} className={`card ${status}`}>
+      <span
+        className={`aspect ${card.info.faction_code}`}
+        title={card.info.faction_name}
+      />
+      {quantity} ×{" "}
+      <a href={card.info.url} target="_blank" rel="noreferrer">
+        {card.info.name}
+      </a>
+      {card.info.is_unique && <span className="unique" title="Unique"></span>}
+    </div>
+  ) : (
+    <div className="card empty">&nbsp;</div>
+  );
+}
+
+function byCardName(a, b) {
+  return a.info.name.localeCompare(b.info.name);
+}
+
+function Header({ deck, remove }) {
+  return (
+    <header>
+      <h4>
+        {deck.info.investigator_name} [{deck.info.id}]
+      </h4>
+      <h2 className={`deck-name ${deck.aspect}`}>{deck.info.name}</h2>
+      <button onClick={remove}>Remove</button>
+    </header>
+  );
+}
+
+function Stats({ deck }) {
+  return (
+    <dl className="stats-list">
+      <dt>Cards:</dt>
+      <dd>{deck.stats.cards}</dd>
+      <dt>Packs:</dt>
+      <dd>{Object.entries(deck.stats.packs).length}</dd>
+      <dt>Shared:</dt>
+      <dd>{deck.stats.shared}</dd>
+      <dt>Danger:</dt>
+      <dd>{deck.stats.danger}</dd>
+      <dt>Warnings:</dt>
+      <dd>{deck.stats.warning}</dd>
+    </dl>
+  );
+}
+
+function Deck({ cards, deck, showConflicts }) {
+  return (
+    <div className="deck">
+      {Object.values(cards)
+        .sort(byCardName)
+        .filter((card) => !showConflicts || card.decks > 1)
+        .map((card) => (
+          <Card
+            card={card}
+            key={card.info.code}
+            quantity={deck.info.slots[card.info.code] || 0}
+          />
+        ))}
+    </div>
+  );
+}
 
 function App() {
+  const [dc, setDc] = useState(false);
+  const [data, setData] = useState({ cards: {}, decks: [] });
+  const [deckId, setDeckId] = useState("");
+  const [showConflicts, setShowConflicts] = useState(false);
+
+  const addDeck = (deckId) => dc.addDeck(deckId).then(setData);
+  const removeDeck = (index) => dc.removeDeck(index).then(setData);
+  const toggleConflicts = (e) => setShowConflicts(e.target.checked);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (deckId) addDeck(deckId);
+    setDeckId("");
+  };
+
+  useEffect(() => {
+    setDc(new DeckCompare());
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setDeckId(e.target.value)} value={deckId} />
+      </form>
+
+      <button onClick={() => addDeck(7692)} className="justice">
+        7692 Scarlet Witch
+      </button>
+      <button onClick={() => addDeck(7801)} className="justice">
+        7801 Scarlet Witch [derived from 7692]
+      </button>
+      <button onClick={() => addDeck(9634)} className="aggression">
+        9634 Black Widow
+      </button>
+      <button onClick={() => addDeck(9871)} className="leadership">
+        9871 Thor
+      </button>
+      <button onClick={() => addDeck(9915)} className="protection">
+        9915 Wasp
+      </button>
+      <button onClick={() => addDeck(10059)} className="leadership">
+        10059 Spider-Man
+      </button>
+      <button onClick={() => addDeck(10060)} className="justice">
+        10060 Spider-Man
+      </button>
+      <button onClick={() => addDeck(10061)} className="leadership">
+        10061 Doctor Strange
+      </button>
+
+      <label>
+        <input type="checkbox" onChange={toggleConflicts} />
+        Show only shared cards
+      </label>
+
+      <div className="headers">
+        {data.decks.map((deck, i) => (
+          <Header
+            deck={deck}
+            key={i}
+            cards={data.cards}
+            remove={() => removeDeck(i)}
+          />
+        ))}
+      </div>
+      <div className="stats">
+        {data.decks.map((deck, i) => (
+          <Stats deck={deck} key={i} cards={data.cards} />
+        ))}
+      </div>
+      <div className="decks">
+        {data.decks.map((deck, i) => (
+          <Deck
+            deck={deck}
+            key={i}
+            cards={data.cards}
+            showConflicts={showConflicts}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/deck-compare/index.js
@@ -0,0 +1,130 @@
+function getInfo(id, type) {
+  const local = localStorage.getItem(`${type}-${id}`);
+  if (local) return Promise.resolve(JSON.parse(local).data);
+  return window
+    .fetch(`https://marvelcdb.com/api/public/${type}/${id}`)
+    .then((response) => response.json())
+    .then((res) => {
+      localStorage.setItem(
+        `${type}-${id}`,
+        JSON.stringify({ date: new Date(), data: res })
+      );
+      return res;
+    });
+}
+
+const getDeckInfo = (id) => getInfo(id, "decklist");
+const getCardInfo = (id) => getInfo(id, "card");
+
+const getDuplicated = (duplicated_by) => {
+  return Promise.all((duplicated_by || []).map(getCardInfo));
+};
+
+const getFullCardInfo = ([id, quantity]) =>
+  getCardInfo(id).then((info) =>
+    getDuplicated(info.duplicated_by || []).then((duplicated_by) => ({
+      info,
+      duplicated_by,
+      quantity,
+      availability:
+        info.quantity + duplicated_by.reduce((a, c) => a + c.quantity, 0),
+    }))
+  );
+
+const getCards = (decks) => {
+  return Promise.all(
+    decks.map((deck) =>
+      Promise.all(Object.entries(deck.info.slots).map(getFullCardInfo))
+    )
+  ).then((deckCards) => {
+    return deckCards.flat().reduce(
+      (cards, card) => ({
+        ...cards,
+        [card.info.code]: {
+          ...card,
+          quantity: (cards[card.info.code]?.quantity || 0) + card.quantity,
+          decks: (cards[card.info.code]?.decks || 0) + 1,
+        },
+      }),
+      {}
+    );
+  });
+};
+
+function getStats(current, value, quantity) {
+  return {
+    ...(current || {}),
+    [value]: (current?.[value] || 0) + quantity,
+  };
+}
+
+function calcStats(cards, deck) {
+  return Object.keys(deck.info.slots).reduce(
+    (current, id) => ({
+      cards: (current.cards || 0) + deck.info.slots[id],
+      shared: (current.shared || 0) + (cards[id].decks > 1),
+      danger:
+        (current.danger || 0) + (cards[id].quantity > cards[id].availability),
+      warning:
+        (current.warning || 0) +
+        (cards[id].decks > 1 && cards[id].info.is_unique),
+      costs: getStats(current.costs, cards[id].info.cost, deck.info.slots[id]),
+      types: getStats(
+        current.types,
+        cards[id].info.type_code,
+        deck.info.slots[id]
+      ),
+      aspects: getStats(
+        current.aspects,
+        cards[id].info.faction_code,
+        deck.info.slots[id]
+      ),
+      packs: getStats(
+        current.packs,
+        cards[id].info.pack_code,
+        deck.info.slots[id]
+      ),
+    }),
+    {}
+  );
+}
+
+function getAspect(stats) {
+  const aspects = ["justice", "leadership", "aggression", "protection"];
+  return Object.entries(stats.aspects)
+    .filter((a) => aspects.includes(a[0]))
+    .sort((a, b) => b[1] - a[1])[0][0];
+}
+
+export class DeckCompare {
+  cards = {};
+  decks = [];
+
+  async updateCards() {
+    return getCards(this.decks).then((cards) => (this.cards = cards));
+  }
+
+  async updateStats() {
+    return (this.decks = this.decks.map((deck) => {
+      const stats = calcStats(this.cards, deck);
+      return { ...deck, stats, aspect: getAspect(stats) };
+    }));
+  }
+
+  async updateData() {
+    return this.updateCards()
+      .then(() => this.updateStats())
+      .then(() => ({ ...this }));
+  }
+
+  async addDeck(id) {
+    return getDeckInfo(id)
+      .then((info) => (this.decks = [...this.decks, { info }]))
+      .then(() => this.updateData());
+  }
+
+  async removeDeck(pos) {
+    this.decks = this.decks.filter((_, i) => i !== pos);
+    return this.updateData();
+  }
+}
